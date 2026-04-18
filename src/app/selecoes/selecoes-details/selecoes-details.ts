0ec619896a10c495acb67bedfba9c08864d0a52c
Duarte099 <a14395@oficina.pt>
2026-04-18 02:20:05 +0100

    Inicio seleções
    Co-authored-by: Diogo_ferreira123 <diogohenriqueferreira5-bot@users.noreply.github.com>

---
+++ src/app/selecoes/selecoes-details/selecoes-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selecoes-details',
+  imports: [],
+  templateUrl: './selecoes-details.html',
+  styleUrl: './selecoes-details.css',
+})
+export class SelecoesDetails {}
---
+++ src/app/selecoes/selecoes-form/selecoes-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selecoes-form',
+  imports: [],
+  templateUrl: './selecoes-form.html',
+  styleUrl: './selecoes-form.css',
+})
+export class SelecoesForm {}
